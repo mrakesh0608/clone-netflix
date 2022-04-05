69618897cefecc3300e80b736ee2090e8132ae05
Merugu Rakesh <101246871+mrakesh0608@users.noreply.github.com>
2022-04-05 12:58:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,21 +3,22 @@ let FA = document.getElementsByClassName('FA');
 let OC = document.getElementsByClassName('oc');
 let HR = document.getElementsByClassName('hr');
 
-
 for (let index = 0; index < FQ.length; index++) 
 {
-    FQ[index].addEventListener('click' , () =>{
-        if (FA[index].style.display === "none") 
-        {
-            FA[index].style.display = "block";
-            HR[index].style.display = "block";
-            OC[index].innerHTML = "x";
-        } 
-        else
-        {
-            FA[index].style.display = "none";
-            HR[index].style.display = "none";
-            OC[index].innerHTML = "+";
-        }
-    });
+    FQ[index].addEventListener('click', ()=>{
+        closeAllFA();
+        FA[index].style.display = "block";
+        HR[index].style.display = "block";
+        OC[index].innerHTML = "x";
+    })
+}
+
+function closeAllFA()
+{
+    for (let index = 0; index < FQ.length; index++) 
+    {
+        FA[index].style.display = "none";
+        HR[index].style.display = "none";
+        OC[index].innerHTML = "+";
+    }
 }
